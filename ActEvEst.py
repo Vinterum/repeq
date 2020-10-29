bbae18dc4cff3426e1797893dc2910612c5b8d34
CDI-Guille <a01733608@itesm.mx>
2020-10-29 04:16:46 +0000

    Se modifico ActEvEst.py

--- ActEvEst.py
+++ ActEvEst.py
@@ -3,9 +3,45 @@ import pandas as pd
 import numpy as np
 # %%
 df = pd.read_csv("ulabox_orders_with_categories_partials_2017.csv")
-df.head()
 
 # %%
-df.describe()
+df.index
 
 # %%
+df.size
+
+# %%
+df.colums
+
+# %%
+df.min()
+
+#%%
+df.max()
+
+# %%
+df.mean()
+
+
+# %%
+df[["customer","total_items","discount%","weekday",
+"hour","Food%","Fresh%","Drinks%","Home%","Beauty%","Health%","Baby%","Pets%"]].mode()
+
+
+# %%
+df.median()
+
+# %%
+df.std()
+
+# %%
+df.quantile([.25,.5,.75])
+
+# %%
+df.quantile(0.5)
+
+# %%
+df.quantile(0.75)
+
+#%%
+df.info()

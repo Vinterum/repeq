4f7c63e11d6832e12e086917cf57f9759bd8e007
Vinterum <A01733902@itesm.mx>
2020-10-30 14:16:58 +0000

    Ya terminé, hay que analizarlo

--- ActEv_Kmeans.py
+++ ActEv_Kmeans.py
@@ -40,16 +40,13 @@ kmeans = KMeans(n_clusters=k).fit(df[["total_items","discount%","weekday","hour"
 #sns.scatterplot(data=df, x="Annual_Income_(k$)", y="Spending_Score", hue="Age")
 
 # %%
+df["Food%_range"]=pd.cut(df["Food%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
+df["Fresh%_range"]=pd.cut(df["Fresh%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
+df["Drinks%_range"]=pd.cut(df["Drinks%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
+df["Home%_range"]=pd.cut(df["Home%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
 df["Beauty%_range"]=pd.cut(df["Beauty%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
-#cluster0['Beauty%_range'].cat.categories=[1,2,3,4,5,6]
-
-# %%
 df["Health%_range"]=pd.cut(df["Health%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
-
-# %%
 df["Baby%_range"]=pd.cut(df["Baby%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
-
-# %%
 df["Pets%_range"]=pd.cut(df["Pets%"], [-1,0,20,40,60,80,100],labels=['0','1-20','21-40','41-60','61-80','81-100'])
 
 # %%
@@ -59,9 +56,44 @@ cluster2=df[kmeans.labels_ == 2]
 cluster3=df[kmeans.labels_ == 3]
 
 # %%
-sns.boxplot(data=cluster0, x="Beauty%_range", y="total_items", hue="Health%_range")
+#A partir de aquí es el cluster 0
+sns.histplot(data=cluster0, x="Baby%_range", multiple="stack", hue="Pets%_range")
 
 # %%
-sns.boxplot(data=cluster0, x="Baby%_range", y="total_items", hue="Beauty%_range")
+sns.histplot(data=cluster0, x="Food%_range", multiple="stack", hue="Fresh%_range")
+#sns.boxplot(data=cluster0, x="Pets%_range", y="total_items", hue="Home%_range")
+
+# %%
+sns.histplot(data=cluster0, x="Home%_range", multiple="stack", hue="Beauty%_range")
+
+# %%
+# A partir de aquí es el cluster 1
+sns.histplot(data=cluster1, x="Baby%_range", multiple="stack", hue="Pets%_range")
+
+# %%
+sns.histplot(data=cluster1, x="Food%_range", multiple="stack", hue="Fresh%_range")
+
+# %%
+sns.histplot(data=cluster1, x="Home%_range", multiple="stack", hue="Beauty%_range")
+
+# %%
+# A partir de aquí es el cluster 2
+sns.histplot(data=cluster2, x="Baby%_range", multiple="stack", hue="Pets%_range")
+
+# %%
+sns.histplot(data=cluster2, x="Baby%_range", multiple="stack", hue="Home%_range")
+
+# %%
+sns.histplot(data=cluster2, x="Baby%_range", multiple="stack", hue="Beauty%_range")
+
+# %%
+# A partir de aquí es el cluster 3
+sns.histplot(data=cluster3, x="Baby%_range", multiple="stack", hue="Pets%_range")
+
+# %%
+sns.histplot(data=cluster3, x="Food%_range", multiple="stack", hue="Drinks%_range")
+
+# %%
+sns.histplot(data=cluster3, x="Home%_range", multiple="stack", hue="Beauty%_range")
 
 # %%
